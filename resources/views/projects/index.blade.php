82981d50cf768d89e1330e6cf89561f4d05c3463
vaishnavvipin12/02/2022 <athiralalithambaran@gmail.com>
2026-07-09 11:28:33 +0530

    edit button

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -163,9 +163,6 @@
                         <div class="inline-flex items-center justify-end gap-1.5 ml-auto">
                             @can('projects.manage')
 
-                                <a href="{{ route('projects.edit', $proj->id) }}" title="Edit Project Details" class="p-2 rounded-lg bg-[#09876B]/10 hover:bg-[#09876B]/20 text-[#09876B] hover:text-[#076852] transition inline-flex items-center justify-center shadow-sm">
-                                    <svg class="w-4 h-4 text-[#09876B]" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/></svg>
-                                </a>
 
                                 <button @click="openEditModal({{ json_encode($proj) }})" title="Edit Project Details" class="p-2 rounded-lg bg-primary/10 hover:bg-primary/20 text-primary hover:text-primary-700 transition inline-flex items-center justify-center shadow-sm">
                                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/></svg>
