3d67c0bb30a2de317ac44ae29f52ebf908bdf28c
Anchumol <anchumol366@gmail.com>
2026-07-22 12:04:37 +0530

    sale exchange

--- resources/views/sales/return_exchange_operations.blade.php
+++ resources/views/sales/return_exchange_operations.blade.php
@@ -1055,9 +1055,9 @@
                                                 <button type="button" @click="selectExchangeSale(sale); newExchangeStep = 2;" class="text-primary hover:text-primary-700 transition-colors" title="Edit Exchange">
                                                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/></svg>
                                                 </button>
-                                                <button type="button" class="text-slate-400 hover:text-slate-650 transition-colors" title="More Options">
+                                                <!-- <button type="button" class="text-slate-400 hover:text-slate-650 transition-colors" title="More Options">
                                                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"/></svg>
-                                                </button>
+                                                </button> -->
                                             </div>
                                         </template>
                                     </div>
